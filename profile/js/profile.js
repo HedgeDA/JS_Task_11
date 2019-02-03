aac067fa33b2207bbbb6e6046f3bb279dbc947ce
HedgeDA <faraon.da@gmail.com>
2019-02-03 22:27:37 +0600

    ...

--- profile/js/profile.js
+++ profile/js/profile.js
@@ -1,1 +1,40 @@
 'use strict';
+
+function onLoadProfile(data) {
+  document.querySelector('[data-name]').textContent = data.name;
+  document.querySelector('[data-description]').textContent = data.description;
+  document.querySelector('[data-pic]').src = data.pic;
+  document.querySelector('[data-position]').textContent = data.position;
+
+  return loadData(`https://neto-api.herokuapp.com/profile/${data.id}/technologies`)
+}
+
+function onLoadTechnologies(data) {
+  document.querySelector('[data-technologies]').innerHTML = data.reduce((innerHTML, item) => {
+    return innerHTML + `<span class="devicons devicons-${item}"></span>`
+  }, '');
+
+  document.querySelector('.content').style.display = 'initial';
+}
+
+function onError(event) {
+  console.log(`Ошибка выполнения скрипта из url: ${event.target.src}`);
+}
+
+function loadData(url) {
+  const functionName = `callback${String(Math.random()).slice(-7)}`;
+
+  return new Promise((resolve, reject) => {
+    window[functionName] = resolve;
+
+    const script = document.createElement('script');
+    script.src = `${url}?callback=${functionName}`;
+    script.onerror = reject;
+    document.body.appendChild(script);
+  });
+}
+
+loadData('https://neto-api.herokuapp.com/profile/me')
+  .then(onLoadProfile)
+  .then(onLoadTechnologies)
+  .catch(onError);
